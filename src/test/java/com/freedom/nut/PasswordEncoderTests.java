13097172c08ce8c00147f67064333cf4487c14e3
Freedom <nuttapon@csc.co.th>
2018-06-12 14:59:56 +0700

    add class

---
+++ src/test/java/com/freedom/nut/PasswordEncoderTests.java
@@ -0,0 +1,15 @@
+package com.freedom.nut;
+
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderTests {
+
+    @Test
+    public void generateEncodedPasswords() {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println("{bcrypt}" + passwordEncoder.encode("secret"));
+        System.out.println("{bcrypt}" + passwordEncoder.encode("secret"));
+        System.out.println("{bcrypt}" + passwordEncoder.encode("secret"));
+    }
+}
